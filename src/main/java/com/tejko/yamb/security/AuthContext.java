04084d156f66322e14fcbdb3084ffe0f9060d5bb
Matej Đanić <matej.danic@triple-innovations.com>
2025-01-06 18:45:34 +0100

    simplify authcontext

--- src/main/java/com/tejko/yamb/security/AuthContext.java
+++ src/main/java/com/tejko/yamb/security/AuthContext.java
@@ -7,18 +7,10 @@ import com.tejko.yamb.domain.models.Player;
 public class AuthContext {
 
     public static Player getAuthenticatedPlayer() {
-        if (isAuthenticated()) {
-            return (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        } else {
-            return null;
-        }
-    }
-
-    private static boolean isAuthenticated() {
         try {
-            return !SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser");
+            return (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         } catch (Exception e) {
-            return false;
+            return null;
         }
     }
 
